65879d919b011622f8532963c466409da6634081
pankaj27 <patrapankaj36@gmail.com>
2024-04-17 12:02:44 +0530

    update 1.2

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -6,10 +6,22 @@ class Signup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign Up'),
-      ),
-      body: Text('Hello'),
-    );
+        appBar: AppBar(
+          title: const Text('Sign up'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+              child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+              )
+            ],
+          )),
+        ));
   }
 }
