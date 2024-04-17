bf73b6fbbf2025f3e1061a0377cd701b20f6c66b
pankaj27 <patrapankaj36@gmail.com>
2024-04-17 12:36:15 +0530

    update 1.4

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
 class Signup extends StatelessWidget {
-  const Signup({super.key});
+  Signup({super.key});
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {}
+  }
+
+  String? _validateEmail(value) {
+    if (value!.isEmpty) {
+      return 'Please enter email';
+    }
+    RegExp emailRegexp = RegExp(r'^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$');
+    if (!emailRegexp.hasMatch(value)) {
+      return 'Please enter valid email';
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,53 +29,62 @@ class Signup extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Form(
+              key: _formKey,
               child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: 'Username',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0))),
-              ),
-              const SizedBox(
-                height: 16.0,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0))),
-              ),
-              const SizedBox(
-                height: 16.0,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: 'Phone no',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0))),
-              ),
-              const SizedBox(
-                height: 16.0,
-              ),
-              SizedBox(
-                height: 50.0,
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.purple,
-                      foregroundColor: Colors.white,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 50, vertical: 5),
-                      textStyle: const TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.bold)),
-                  child: const Text('Submit',
-                      style: TextStyle(color: Colors.white)),
-                ),
-              ),
-            ],
-          )),
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter username';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                    validator: _validateEmail,
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Phone no',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  SizedBox(
+                    height: 50.0,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: _submitForm,
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.purple,
+                          foregroundColor: Colors.white,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 5),
+                          textStyle: const TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold)),
+                      child: const Text('Submit',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                  ),
+                ],
+              )),
         ));
   }
 }
