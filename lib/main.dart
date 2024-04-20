d3db70ca97f81d74159e63c0956c8dba41637958
pankaj27 <patrapankaj36@gmail.com>
2024-04-20 12:51:02 +0530

    update 2.1

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,10 @@ import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
